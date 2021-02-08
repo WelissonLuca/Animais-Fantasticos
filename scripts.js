da99bdff4229cb2331c1a7eedbf867a24fb07adc
Welisson Luca <welissonluca17@gmail.com>
2021-02-08 19:14:03 -0300

    Animacao scroll completa

--- scripts.js
+++ scripts.js
@@ -57,6 +57,24 @@ function initScrollSuave() {
 }
 
 ////
+function initAnimacaoScroll() {
+    const sections = document.querySelectorAll(".js-scroll");
+    if (sections.length) {
+      const windowMetade = window.innerHeight * 0.6;
+      function animaScroll() {
+        sections.forEach((section) => {
+          const sectionTop = section.getBoundingClientRect().top;
+          const isSectionVisible = sectionTop - windowMetade < 0;
+          if (isSectionVisible) section.classList.add("ativo");
+          else section.classList.remove("ativo");
+        });
+      }
+      window.addEventListener("scroll", animaScroll);
+    }
+}
+
+
 initTabNav();
 initAccordion();
 initScrollSuave();
+initAnimacaoScroll();
