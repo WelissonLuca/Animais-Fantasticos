89c32188d8bb719a196193cf860a1e9072346e37
Welisson Luca <welissonluca17@gmail.com>
2021-02-24 01:23:01 -0300

    feat: modules create

---
+++ js/script.js
@@ -0,0 +1,8 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from "./modules/scroll-animation.js";
+import initAccordion from "./modules/accordion-animate.js";
+import initTabNav from "./modules/tab-navigation.js";
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
