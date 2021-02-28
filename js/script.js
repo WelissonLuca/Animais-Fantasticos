023c5f44177ca4e1906ac36fc9415acb17b1cfe5
Welisson Luca <welissonluca17@gmail.com>
2021-02-28 12:11:46 -0300

    fix: all bugs modules

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 import initScrollSuave from './modules/scroll-suave.js';
-import initAnimacaoScroll from "./modules/scroll-animation.js";
-import initAccordion from "./modules/accordion-animate.js";
-import initTabNav from "./modules/tab-navigation.js";
-import initTooltip from './modules/tooltip.js'
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
+initModal();
 initTooltip();
