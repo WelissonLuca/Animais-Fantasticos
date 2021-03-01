fd067de3e402f8a3bad18c9336401ec7f18bd56a
Welisson Luca <welissonluca17@gmail.com>
2021-03-01 16:45:35 -0300

    feat OutsideCLick create

--- js/modules/dropdowm-menu.js
+++ js/modules/dropdowm-menu.js
@@ -1,19 +1,18 @@
-export default function initDropDowmMenu(){
-    
-}
+import outsideClick from "./outsideClick";
+export default function initDropDowmMenu() {}
 
-
-const dropdowmMenus = document.querySelectorAll('[data-dropdowm') /
-
-dropdowmMenus.forEach(menu => {
-  
-    ['touchstart', 'click'].forEach(userEvent => {
-          menu.addEventListener("click", userEvent);
-    })
-})
+const dropdowmMenus =
+	document.querySelectorAll("[data-dropdowm") /
+	dropdowmMenus.forEach((menu) => {
+		["touchstart", "click"].forEach((userEvent) => {
+			menu.addEventListener("click", userEvent);
+		});
+	});
 
 function handleClick(event) {
-    event.preventDefaul();
-    this.classList.toogle('active')
-
+	event.preventDefaul();
+	this.classList.add("active");
+	outsideClick(this, ["touchstart", "click"], () => {
+		this.classList.remove("active");
+	});
 }
